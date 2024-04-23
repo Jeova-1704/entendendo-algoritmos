88dc48b604550aaa1ceca4b91a763a9ecd91ef28
Jeova BL <jeovab115@gmail.com>
2024-04-23 19:39:40 -0300

    vetor python

--- lista_encadeada/lista_encadeada.py
+++ lista_encadeada/lista_encadeada.py
@@ -2,53 +2,56 @@ class No:
     def __init__(self, valor) -> None:
         self.__valor = valor
         self.__proximo = None
-        
-    def get_valor(self):
+
+    @property
+    def valor(self):
         return self.__valor
-    
-    def get_proximo(self):
+
+    @property
+    def proximo(self):
         return self.__proximo
     
-    def set_proximo(self, proximo):
+    @proximo.setter
+    def proximo(self, proximo):
         self.__proximo = proximo
-        
+
 
 class ListaEncadeada:
-    
+
     def __init__(self) -> None:
         self.__inicio = None
         self.__fim = None
-        
+
     def inserir_final(self, valor):
         novo_elemento = No(valor)
-        
-        if (self.__inicio == None):
+
+        if self.__inicio == None:
             self.__inicio = novo_elemento
             self.__fim = novo_elemento
-            
+
         else:
             self.__fim.set_proximo(novo_elemento)
             self.__fim = novo_elemento
-    
+
     def inserir_inicio(self, valor):
         novo_elemento = No(valor)
-        
-        if (self.__inicio == None):
+
+        if self.__inicio == None:
             self.__inicio = novo_elemento
             self.__fim = novo_elemento
-            
+
         else:
             novo_elemento.set_proximo(self.__inicio)
             self.__inicio = novo_elemento
-            
+
     def inserir_ordenado(self, valor):
         novo_elemento = No(valor)
-        
-        if (self.__inicio is None):
+
+        if self.__inicio is None:
             self.__inicio = novo_elemento
             self.__fim = novo_elemento
             return
-            
+
         elif valor < self.__inicio.get_valor():
             novo_elemento.set_proximo(self.__inicio)
             self.__inicio = novo_elemento
@@ -57,25 +60,24 @@ class ListaEncadeada:
         else:
             atual = self.__inicio
             anterior = No(None)
-            
-            while (atual is not None and atual.get_valor() < valor):
+
+            while atual is not None and atual.get_valor() < valor:
                 anterior = atual
                 atual = atual.get_proximo()
 
             anterior.set_proximo(novo_elemento)
             novo_elemento.set_proximo(atual)
-                
+
     def printar(self):
         atual = self.__inicio
-        while (atual is not None):
+        while atual is not None:
             print(atual.get_valor(), end=" ")
             atual = atual.get_proximo()
-            
-            
-        
+
+
 if __name__ == "__main__":
     lista_encadeada = ListaEncadeada()
-    
+
     lista_encadeada.inserir_ordenado(5)
     lista_encadeada.inserir_ordenado(3)
     lista_encadeada.inserir_ordenado(1)
---
+++ vetor/vetor.py
@@ -0,0 +1,91 @@
+import numpy as np
+
+
+class VetorVazioException(Exception):
+    def __init__(self, mensagem) -> None:
+        self.mensagem = mensagem
+
+    def __str__(self):
+        return repr(self.mensagem)
+
+
+class VetorMaxCapacidadeException(Exception):
+    def __init__(self, mensagem) -> None:
+        self.mensagem = mensagem
+
+    def __str__(self):
+        return repr(self.mensagem)
+
+
+class VetorTipoException(Exception):
+    def __init__(self, mensagem) -> None:
+        self.mensagem = mensagem
+
+    def __str__(self):
+        return repr(self.mensagem)
+
+
+class VetorNaoOrdenado:
+    def __init__(self, capacidade, tipo) -> None:
+        self.capacidade = capacidade
+        self.ultima_posicao = -1
+        self.tipo = tipo
+        self.valores = np.empty(self.capacidade, dtype=self.tipo)
+
+    # O(n)
+    def imprime(self):
+        if self.ultima_posicao == -1:
+            raise VetorVazioException("O vetor está vazio")
+        else:
+            for i in range(self.ultima_posicao + 1):
+                print(i, " - ", self.valores[i])
+
+    #O(1)
+    def insere(self, valor):
+        if self.ultima_posicao == self.capacidade - 1:
+            raise VetorMaxCapacidadeException("Capacidade maxima do vetor atingida")
+
+        if not isinstance(valor, self.tipo):
+            raise VetorTipoException(f"Tipo do valor não compativél com o tipo do vetor! Apenas elementos do tipo {self.tipo} são permitidos")
+        
+        else:
+            self.ultima_posicao += 1
+            self.valores[self.ultima_posicao] = valor
+    
+    #O(n)
+    def pesquisar(self, valor):
+        for i in range(self.ultima_posicao + 1):
+            if (valor == self.valores[i]):
+                return i
+        return None
+    
+    #O(n)
+    def excluir(self, valor):
+        posicao = self.pesquisar(valor)
+        
+        if (posicao == None):
+            return None
+        
+        else:
+            for i in range(posicao, self.ultima_posicao - 1):
+                self.valores[i] = self.valores[i + 1]
+                
+            self.ultima_posicao -= 1
+        
+
+
+if __name__ == "__main__":
+    vetor = VetorNaoOrdenado(5, int)
+    vetor.insere(2)
+    vetor.insere(3)
+    vetor.insere(4)
+    vetor.insere(5)
+    vetor.insere(6)
+    print(vetor.ultima_posicao)
+    vetor.imprime()
+    print(vetor.pesquisar(4))
+    vetor.excluir(6)
+    vetor.imprime()
+    print(vetor.ultima_posicao)
+    
+    
